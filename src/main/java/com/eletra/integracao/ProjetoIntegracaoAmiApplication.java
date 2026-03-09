4d857a81d025aaee6541fb32da4c1ad415512301
João Victor Silva França <joaovictorbass12@gmail.com>
2026-03-09 09:17:11 -0300

    chore: initial project setup with Spring Boot, Docker, Redis, PostgreSQL and ActiveMQ

---
+++ src/main/java/com/eletra/integracao/ProjetoIntegracaoAmiApplication.java
@@ -0,0 +1,13 @@
+package com.eletra.integracao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegracaoAmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegracaoAmiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eletra/integracao/ProjetoIntegracaoAmiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eletra.integracao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntegracaoAmiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
